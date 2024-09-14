51d0ff49bfd475a053bea1445667e880102f9a36
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-09-14 22:51:59 +0100

    implement support for the PING command.

--- src/main/java/com/zaaim/kv/Main.java
+++ src/main/java/com/zaaim/kv/Main.java
@@ -9,19 +9,22 @@ public class Main {
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
-        int port = 6379;
+        int port = 6123;
         try {
             serverSocket = new ServerSocket(port);
 
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+            // Write to the client
+            clientSocket.getOutputStream().write("+O Right\r\n".getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
             try {
                 if (clientSocket != null) {
                     clientSocket.close();
+                    System.out.println("Client socket closed");
                 }
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
